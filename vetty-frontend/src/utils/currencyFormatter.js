ce46ccb180c3a880b8be23b8a53c83266dc7cbda
Victor <victor.kihiko@student.moringaschool.com>
2025-05-13 14:01:57 +0300

    removed folder

--- vetty-frontend/src/utils/currencyFormatter.js
+++
@@ -1,9 +0,0 @@
-export function formatCurrency(amount) {
-  if (typeof amount !== 'number') {
-    return '';
-  }
-  return new Intl.NumberFormat('en-KE', {
-    style: 'currency',
-    currency: 'KES',
-  }).format(amount);
-}
